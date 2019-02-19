785565e15dc590f8a511e32905fd9106ca101e8b
Sabeeha tanveer <sabeeha.tanveer9@gmail.com>
2019-02-19 23:32:14 +0500

    second commit

---
+++ app/src/main/java/com/example/naveed/backup/TabPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.example.naveed.backup;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabPagerAdapter extends FragmentPagerAdapter {
+
+
+    public TabPagerAdapter(FragmentManager fm, int tabCount) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                contactFragment contact= new contactFragment();
+                return contact;
+            case 1:
+                msgFragment msg= new msgFragment();
+                return msg;
+             default:
+                 return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Contact";
+            case 1:
+                return "SMS";
+            default:
+                return null;
+        }
+    }
+}
